0e96ff330e3995cfcc05d1970e99cb0959ea5f44
Mike Cohen <mjoecohen@gmail.com>
2020-07-30 11:25:51 +0100

    bug fix - mp4 bug minified lsat module

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ if [ -z "${SERVICE}" ]; then
   rm -rf $PATH_DEPLOY/*
   BUILD_PATH=../fe-lsat
   __build
-  cp $BUILD_PATH/dist/lsat-entry.js $PATH_DEPLOY/.
+  cp $BUILD_PATH/dist/lsat-entry*.js $PATH_DEPLOY/.
 
   BUILD_PATH=./
   __build
@@ -32,7 +32,7 @@ fi
 if [ "$SERVICE" == "lsat" ]; then
   BUILD_PATH=../fe-lsat
   __build
-  cp $BUILD_PATH/dist/lsat-entry.js $PATH_DEPLOY/.
+  cp $BUILD_PATH/dist/lsat-entry*.js $PATH_DEPLOY/.
 fi
 if [ "$SERVICE" == "risidio" ]; then
   BUILD_PATH=./
